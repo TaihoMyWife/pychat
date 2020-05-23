d33bce96cc12f7860f19827f72fe1d3c7f21910f
TaihoMyWife <TaihoMyWife@hotmail.com>
2020-05-23 11:58:32 +0800

    test7

--- client/ui_chat_b.py
+++ client/ui_chat_b.py
@@ -2,7 +2,8 @@
 
 # Form implementation generated from reading ui file 'chat.ui'
 #
-# Created by: PyQt4 UI code generator 4.12.1
+# Created: Fri Apr 18 23:16:51 2014
+#      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -22,8 +23,8 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
-class Ui_Dialog(object):
-    def setupUi(self, Dialog):
+class Ui_DialogChat(object):
+    def __init__(self, Dialog):
         Dialog.setObjectName(_fromUtf8("Dialog"))
         Dialog.resize(540, 420)
         palette = QtGui.QPalette()
@@ -149,10 +150,11 @@ class Ui_Dialog(object):
         QtCore.QObject.connect(self.cleanButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.textBrowser.clear)
         QtCore.QObject.connect(self.rewriteButton, QtCore.SIGNAL(_fromUtf8("clicked()")), self.textEdit.clear)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.Dialog = Dialog
 
     def retranslateUi(self, Dialog):
-        Dialog.setWindowTitle(_translate("Dialog", "世豪", None))
-        Dialog.setToolTip(_translate("Dialog", "世豪", None))
+        Dialog.setWindowTitle(_translate("Dialog", "亮剑", None))
+        Dialog.setToolTip(_translate("Dialog", "亮剑", None))
         self.closeButton.setText(_translate("Dialog", "关闭", None))
         self.rewriteButton.setText(_translate("Dialog", "重写", None))
         self.sendButton.setText(_translate("Dialog", "发送", None))
